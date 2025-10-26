fe0ed42a9cbe08f396550b845b99aba821f5fd34
joncru <jonathan.h.cruz@live.com>
2025-10-26 15:14:50 -0500

    add-mobile-version

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,7 @@
-console.log('U Compensar page loaded successfully!');
+
+const mobileMenuButton = document.querySelector('.menu__mobile-nav');
+const mobileMenu = document.querySelector('.menu__nav');
+
+mobileMenuButton.addEventListener('click', () => {
+    mobileMenu.classList.toggle('menu__nav--active');
+})
